f739bab6fd4295a4e78d1781854b798d0d94e406
sadhana235 <sadhana.shashidhar@gmail.com>
2023-11-22 14:34:13 +0530

    integrate AR iframe rendering using Echo3D

--- src/app/restaurants/[restaurant]/page.tsx
+++ src/app/restaurants/[restaurant]/page.tsx
@@ -7,6 +7,12 @@ import { useParams } from "next/navigation";
 const restaurantName = "Restaurant Name";
 const menuFilters = ["Veg", "All", "Appetizers", "Entrees", "Desserts"];
 
+const [cart, setCart] = useState([]);
+
+
+
+
+
 // const foodItems = [
 //   {
 //     id: 1,
@@ -56,14 +62,24 @@ interface Menu {
   description: string;
   price: number;
   image: string;
+  url: string;
 }
 
 const IndexPage = () => {
-//   const handleViewInAR = (foodItem) => {
-//     // Handle view in AR functionality
-//     console.log(`Viewing ${foodItem.name} in AR...`);
-//   };
+  //   const handleViewInAR = (foodItem) => {
+  //     // Handle view in AR functionality
+  //     console.log(`Viewing ${foodItem.name} in AR...`);
+  //   };
   // const { _id } = useParams();
+  //Creating states to show iframe when url givr
+  const [showIframe, setShowIframe] = useState(false);
+  const [iframeSrc, setIframeSrc] = useState("");
+
+  const handleViewIn3D = (url: string) => {
+    setIframeSrc(url);
+    setShowIframe(!showIframe);
+  };
+
   const params = useParams();
   const _id = params.restaurant;
 
@@ -82,7 +98,7 @@ const IndexPage = () => {
       .catch((error) => console.log('Fetching food items failed: ', error));
   }, [_id]);
   console.log(foodItems);
-  
+
 
   return (
     <div>
@@ -110,6 +126,7 @@ const IndexPage = () => {
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-10">
           {foodItems.map((foodItem) => (
+
             <div
               key={foodItem.id}
               className="bg-white shadow-md rounded-md overflow-hidden"
@@ -127,21 +144,21 @@ const IndexPage = () => {
                 <div className="flex items-center justify-between mt-4">
                   <span className="text-gray-500">${foodItem.price}</span>
                   <div>
-                    <button
-                      className="bg-gray-800 text-white px-4 py-2 rounded-md mr-2"
-                    //   onClick={() => handleViewInAR(foodItem)}
-                    >
+                    <button  onClick={() => handleViewIn3D(foodItem.url)} className="bg-gray-800 text-white px-4 py-2 rounded-md mr-2">
                       View in AR/3D
                     </button>
+
                     <button className="bg-gray-800 text-white px-4 py-2 rounded-md">
                       Add to Cart
                     </button>
                   </div>
+                  {/* {url && <iframe src={url} width="500" height="500"></iframe>} */}
                 </div>
               </div>
             </div>
           ))}
         </div>
+        {showIframe && <iframe src={iframeSrc} width="500" height="500"></iframe>}
       </div>
     </div>
   );
